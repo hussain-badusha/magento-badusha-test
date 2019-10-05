d3e380badc8ed70513933b1475cccf21e292c602
Hussain <hussain@plestar.net>
2019-10-05 11:33:01 +0530

    Setup install schema added

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,56 @@
+<?php
+namespace Badusha\Test\Setup;
+
+class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
+{
+	public function install(\Magento\Framework\Setup\SchemaSetupInterface $setup, \Magento\Framework\Setup\ModuleContextInterface $context)
+	{
+		$installer = $setup;
+		$installer->startSetup();
+		if (!$installer->tableExists('badusha_test_table')) {
+			$table = $installer->getConnection()->newTable(
+				$installer->getTable('badusha_test_table')
+			)
+				->addColumn(
+					'id',
+					\Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+					null,
+					[
+						'identity' => true,
+						'nullable' => false,
+						'primary'  => true,
+						'unsigned' => true,
+					],
+					'Post ID'
+				)
+				->addColumn(
+					'name',
+					\Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+					255,
+					['nullable => false'],
+					'Post Name'
+				)
+				->addColumn(
+					'created_at',
+					\Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+					null,
+					['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+					'Created At'
+				)
+				->setComment('Badusha Test Table');
+			$installer->getConnection()->createTable($table);
+
+			$installer->getConnection()->addIndex(
+				$installer->getTable('badusha_test_table'),
+				$setup->getIdxName(
+					$installer->getTable('badusha_test_table'),
+					['name'],
+					\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+				),
+				['name'],
+				\Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+			);
+		}
+		$installer->endSetup();
+	}
+}
